fd61b34e337b761526506815934527ae19f4072a
Kumaresan Manickavelu <kumaresan.manickavelu@gmail.com>
2016-10-31 15:28:20 -0700

    added a loop

--- src/main.go
+++ src/main.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
-    fmt.Println("hello world")
+	
+	for i := 0; i < 600 ; i++ {		
+	    fmt.Println("hello world ", i)
+	    time.Sleep(time.Second)
+	}
 }
